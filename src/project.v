4f5d7e923c43cc00706856d28d7f81b53c11f5cd
David Parent <david.parent@sjsu.edu>
2024-05-29 14:08:30 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,6 +18,7 @@ module tt_um_davidparent_hdl (
   reg [7:0] counter;    // Counter for generating frequency
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign counter =  ui_in
   assign uo_out  = counter;
     assign uio_out = 0;
   assign uio_oe  = 0;
