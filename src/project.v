6372123357afda3c34de55388a7cbf3866133c5b
David Parent <david.parent@sjsu.edu>
2024-05-29 15:29:13 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -37,11 +37,11 @@ end
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
  
-  assign uo_out[0] =1'b0;
+  assign uo_out[0] =counter[0] ;
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-    wire _unused = &{ena, uio_in, ui_in,uo_out[7:1] 1'b0}; 
+    wire _unused = &{ena, uio_in, ui_in, 1'b0}; 
 
 endmodule
