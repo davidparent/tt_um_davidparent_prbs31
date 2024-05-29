12f36280fab93164a1bfdf291d9d7903909ad5e7
David Parent <david.parent@sjsu.edu>
2024-05-29 13:49:18 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -34,7 +34,7 @@ always @(posedge clk ) begin
     end
 end
 
-endmodule
+
   // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out[7:1] = 0;
