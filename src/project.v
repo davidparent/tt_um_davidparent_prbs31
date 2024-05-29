7fec2555e5ceb406dc152072f59988fc81e1d10f
David Parent <david.parent@sjsu.edu>
2024-05-29 14:04:19 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,10 +15,11 @@ module tt_um_davidparent_hdl (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+  reg [7:0] counter;    // Counter for generating frequency
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out  = counter;
+    assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
