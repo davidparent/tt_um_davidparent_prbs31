6d0195bc4f827a326a73f59edef9cc0a2538225d
David Parent <david.parent@sjsu.edu>
2024-05-30 10:27:22 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -32,7 +32,7 @@ end
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
  
-  assign uo_out[0] =lfsr[31] ;
+  assign uo_out[0] =lfsr[30] ;
   assign uio_out = 0;
   assign uio_oe  = 0;
   assign uo_out[7:1]= 7'd0;
