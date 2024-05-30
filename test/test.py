edc2b9e70f74d2500a6d4339bf83450a3f15dc8d
David Parent <david.parent@sjsu.edu>
2024-05-30 16:00:45 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -22,7 +22,7 @@ async def test_project(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-     await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 0
     dut._log.info("Test project behavior")
 
