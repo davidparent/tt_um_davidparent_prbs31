2cc81cf8ca10fcd8607b3f89702a6304990385e8
David Parent <david.parent@sjsu.edu>
2024-06-01 09:41:36 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -14,13 +14,17 @@ module tt_um_davidparent_hdl (
     input  wire       rst_n     // reset_n - low to reset
 );
     reg [30:0] lfsr;
+    reg [30:0] lfsr_test;
     always @(posedge clk or posedge rst_n) begin
         if (rst_n) begin
         lfsr <= 31'd1; 
+        lfsr_test <= 31'd1;     
     end else begin
         // Increment counter on each clock cycle
         lfsr[0] <= lfsr[27] ^ lfsr[30] ;
         lfsr[30:1] <=lfsr[29:0] ;  
+        lfsr[0] <= lfsr_test[27] ^ lfsr_test[30] ;
+        lfsr[30:1] <=lfsr_test[29:0] ;  
     end
 end  
   // All output pins must be assigned. If not used, assign to 0. 
