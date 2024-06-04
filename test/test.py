9da1f37b17e458381690784bf0f74485ed4c39df
davidparent <david.parent@sjsu.edu>
2024-06-04 09:05:29 -0700

    4th try

--- test/test.py
+++ test/test.py
@@ -4,31 +4,33 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-import numpy as np
+
 prbs_size=31 #Size of the LSFR
 #Fill two numpy ararys with 1's
-PRBSN=np.ones(prbs_size)
-PRBSO=np.zeros(prbs_size)
-ttt=prbs_size-1
-PRBSO[prbs_size-1]=int(1)
-
+PRBSN=[0]*prbs_size
+PRBSO=[0]*prbs_size
+PRBSO[prbs_size-1]=1
 #Number of clock cycles:
-n_clock=1000
+n_clock=10000
 #set input and output arrays
-out=np.ones(n_clock)
+out=[1]*(n_clock)
 input=out
 for i in range(n_clock):
   #input the feedback
-  PRBSN[0]=np.logical_xor(PRBSO[27],PRBSO[30])
+  PRBSN[0]=PRBSO[27]^PRBSO[30]
   #shift the vlaues
-  for j in range(len(PRBSN)-1):
-    count=len(PRBSN)-j-1
+  for j in range(prbs_size-1):
+    count=prbs_size-j-1
     PRBSN[count]=PRBSO[count-1]
-  #update the arrary
+  #update the array
+
   for j in range(len(PRBSN)):
     PRBSO[j]=PRBSN[j]
+    
   #tqke the output from the rightmost FF.
-  out[i]=int(PRBSN[prbs_size-1])
+  out[i]=PRBSN[prbs_size-1]
+   
+
 
 @cocotb.test() 
 async def test_project(dut):
